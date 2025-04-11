2f3dc6f9d6503b21db9f99d2b1887cd3a693470f
Giovanni Mazzone <giovannimariamazzone@gmail.com>
2025-04-12 00:21:15 +0200

    Fix build

--- app/java-llama.cpp/.github/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-mkdir build
-cmake -Bbuild %*
-cmake --build build --config Release
-
-if errorlevel 1 exit /b %ERRORLEVEL%
--- app/java-llama.cpp/.github/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-cmake -Bbuild $@ || exit 1
-cmake --build build --config Release -j4 || exit 1
